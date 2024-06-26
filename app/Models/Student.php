686c5ff47b866b3b706261dc5f3937036b91e291
sbachri17 <syaifulbachri091213@gmail.com>
2024-06-26 11:24:47 +0700

    add  function jurusan

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-    protected $fillable = ['nis','name','birth_date',];
+    protected $fillable = ['nis','name','birth_date','jurusan_id'];
+
+    function jurusan(){
+        return $this->belongsTo(Jurusan::class);
+    }
 }
